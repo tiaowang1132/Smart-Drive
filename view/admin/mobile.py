4009e730a5d30d8fbabe5e1ecd714002b2f885d0
tiaowang <tiaowang1132@gmail.com>
2021-12-06 12:24:48 +0800

    Delete view directory

--- view/admin/mobile.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, render_template
-admin_Mobile = Blueprint('adminMobile', __name__, url_prefix='/admin/mobile')
-
-
-#  图片管理
-@admin_Mobile.get('/upload')
-def upload():
-    return render_template('mobile/index.html')
