99c2b6ef743dcb2ab9292eafc14eae20cb22077e
tiaowang <tiaowang1132@gmail.com>
2021-12-06 12:24:10 +0800

    Delete schemas directory

--- schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .admin_user import UserOutSchema
-from .admin_role import RoleOutSchema
-from .admin_power import PowerOutSchema, PowerOutSchema2
-from .admin_log import LogOutSchema
-from .admin_photo import PhotoOutSchema
